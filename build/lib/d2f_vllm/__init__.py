9a1e18b4b2f4616cf51eed061e7fb8dc9f77b3af
Drew <drewjin0827@gmail.com>
2025-08-12 15:31:58 +0800

    build: add build files into .gitignore

--- build/lib/d2f_vllm/__init__.py
+++
@@ -1,2 +0,0 @@
-from d2f_vllm.llm import LLM
-from d2f_vllm.sampling_params import SamplingParams
--- build/lib/d2f_vllm/llm.py
+++
@@ -1,5 +0,0 @@
-from d2f_vllm.engine.llm_engine import LLMEngine
-
-
-class LLM(LLMEngine):
-    pass
