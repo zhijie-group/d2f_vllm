81a81f60f626c10d80f10ffefcfabc3b873c1111
drewjin <drewjin0827@gmail.com>
2025-08-20 20:33:50 +0800

    test: modify test file arguments

--- scripts/test_dvllm_dream_gsm8k.sh
+++ scripts/test_dvllm_dream_gsm8k.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/zsh
-CUDA_VISIBLE_DEVICES=3,4 python examples/test_dream_dvllm_gsm8k.py 2>&1 | tee log/test_dvllm_dream_gsm8k.log
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python examples/test_dream_dvllm_gsm8k.py 2>&1 | tee log/test_dvllm_dream_gsm8k.log
